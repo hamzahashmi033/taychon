e879c0c1d6971ac4cdc7b9b04609631179e3a12b
uzair <muzair2k19@gmail.com>
2025-03-02 04:27:23 +0500

    first 4 pages

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("5e8a2650792672a0")
+/******/ 		__webpack_require__.h = () => ("215faa43845c54b8")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- components/project-details/Challenge.jsx
+++ components/project-details/Challenge.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Challenge() {
+function Challenge({title, description}) {
   return (
     <section className="section-padding">
       <div className="container">
@@ -8,24 +8,15 @@ function Challenge() {
           <div className="col-lg-11">
             <div className="row">
               <div className="col-lg-5">
-                <h4 className="mb-50">01 . The Challenge</h4>
+                <h4 className="mb-50">{title}</h4>
               </div>
               <div className="col-lg-7">
                 <div className="text">
                   <h5 className="mb-30 fw-400 line-height-40">
-                    The goal is there are many variations of passages of Lorem
-                    Ipsum available, but the majority have suffered alteration
-                    in some form, by injected humour, or randomised words which
-                    don&lsquo;t look even slightly believable.
+                    {description}
                   </h5>
                   <p className="fz-18">
-                    There are many variations of passages of Lorem Ipsum
-                    available, but the majority have suffered alteration in some
-                    form, by injected humour, or randomised words which
-                    don&lsquo;t look even slightly believable. If you are going
-                    to use a passage of Lorem Ipsum, you need to be sure there
-                    isn&lsquo;t anything embarrassing hidden in the middle of
-                    text.
+                    
                   </p>
                 </div>
               </div>
