5f75ca67d902b546ba31dbc04e4193f22d6145de
hamzahashmi033 <hashmihamza846@gmail.com>
2025-03-02 23:18:29 +0500

    pushing it on vercel

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
-const path = require('path');
+const path = require("path");
 
 const nextConfig = {
-  output: 'export',
-
   reactStrictMode: false,
   sassOptions: {
-    includePaths: [path.join(__dirname, 'css')],
+    includePaths: [path.join(__dirname, "css")],
   },
   trailingSlash: true,
   devIndicators: {
